06de58449028a99c4e1f1461340cfcb56554b0e4
Lara <Laraewellen@gmail.com>
2025-04-09 01:47:50 -0300

    verificando idade

---
+++ verificadoracesso.py
@@ -0,0 +1,14 @@
+Nome = input("Digite o seu nome: ")
+try: 
+    Idade = int (input("Digite sua idade: "))
+
+    if Idade < 0:
+        print("Idade inválida")
+
+    elif Idade < 18:
+        print(f"Acesso negado, é nescessário ter no mínimo 18 anos")
+
+    else:
+        print(f"Acesso autorizado, bem vindo(a) {Nome}")
+except ValueError:
+    print("Somente números são válidos para a idade")
