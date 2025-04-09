80f7528544af1b6e70001d0c48dafee3752c9c86
Lara <Laraewellen@gmail.com>
2025-04-09 02:21:42 -0300

    verificando acesso

--- verificadoracesso.py
+++ verificadoracesso.py
@@ -3,22 +3,24 @@ Nome = input("Usuário: ")
 
 Senha = input("Senha: ")
 
-senhacerta = '1234'
+senha_correta = "1234"
 
-if Senha == senhacerta:
-    print(f"Bem vindo(a) {Nome}")
-    try: 
-        Idade = int (input("Digite sua idade: "))
+while Senha != senha_correta:
+    print("Senha incorreta, tente novamente")
+    Senha = input("senha: ")
+    
+print(f"Bem vindo(a) {Nome}")
+try: 
+    Idade = int (input("Digite sua idade: "))
 
-        if Idade < 0:
-            print("Idade inválida")
+    if Idade < 0:
+        print("Idade inválida")
 
-        elif Idade < 18:
-            print(f"Acesso negado, é nescessário ter no mínimo 18 anos")
+    elif Idade < 18:
+        print(f"Acesso negado, é nescessário ter no mínimo 18 anos")
 
-        else:
-            print(f"Acesso autorizado, bem vindo(a) {Nome}")
-    except ValueError:
+    else:
+        print(f"Acesso autorizado, bem vindo(a) {Nome}")
+except ValueError:
         print("Somente números são válidos para a idade")
-else:
-    print("Senha errada!")
+
